ff8ea9040ed9c15c2ee193247fd88d3f84bd0570
Sven Liebert <sven.liebert@telekom.de>
2025-08-09 21:13:19 +0200

    Correct backend login and get ce behind felogin

--- tests/tests/page/login-to-backend.spec.ts
+++ tests/tests/page/login-to-backend.spec.ts
@@ -4,7 +4,7 @@ test.describe('Log in into TYPO3 backend', () => {
     test('should be logged in', async ({ page }) => {
         await page.goto('/typo3')
         await page.getByPlaceholder('Username').fill(process.env.USERNAME)
-        await page.getByPlaceholder('Password').fill("wrong")
+        await page.getByPlaceholder('Password').fill(process.env.PASSWORD)
         await page.getByRole('button', { name: 'Login' }).click()
 
         await expect(page.locator('.t3js-topbar-button-modulemenu')).toBeVisible();
